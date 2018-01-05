f283aff78a420aa07bb228cb238646254b5d77b3
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-01-05 13:02:25 +0800

    roles/group UI

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder  } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
@@ -8,9 +9,27 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class RolesComponent implements OnInit {
 
+  updateForm: FormGroup
+
+  rolespermission = ['admin | log entry | Can add log entry', 'admin | log entry | Can change log entry', 
+  'admin | log entry | Can delete log entry', 'announcement | announcement | Can change announcement', 
+  'announcement | announcement | Can delete announcement', 'announcement | announcement category | Can add announcement category'];
+
+  rolesname: FormControl  
+  permission: FormControl
+
   constructor() { }
 
   ngOnInit() {
+    
+    this.rolesname = new FormControl()
+    this.permission = new FormControl()
+
+    this.updateForm = new FormGroup({   
+
+      rolesname: this.rolesname,
+      permission: this.permission,
+    });
   }
 
 }
