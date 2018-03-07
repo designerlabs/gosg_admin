f538542ce7c5d75ac7fd9e83273c4aa3753fc165
redza.rosli <redza.rosli@mimos.my>
2018-03-07 11:35:46 +0800

    updated modmenu

--- src/app/modmenu/modmenutbl/modmenutbl.component.ts
+++ src/app/modmenu/modmenutbl/modmenutbl.component.ts
@@ -56,6 +56,10 @@ export class ModmenutblComponent implements OnInit {
   
   }
 
+  resetSearch() {
+    this.getModuleData(this.pageCount, this.pageSize);
+  }
+
   constructor(
     private http: HttpClient, 
     @Inject(APP_CONFIG) private appConfig: AppConfig, 
