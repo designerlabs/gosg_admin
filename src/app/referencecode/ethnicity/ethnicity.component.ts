b9f459082650e27ea62556f7fc0b1fb1cea07448
redza.rosli <redza.rosli@mimos.my>
2018-03-07 18:17:23 +0800

    updated race

--- src/app/referencecode/ethnicity/ethnicity.component.ts
+++ src/app/referencecode/ethnicity/ethnicity.component.ts
@@ -91,10 +91,10 @@ export class EthnicityComponent implements OnInit {
     }
     
     // #### for disable non update user ---1
-    if(!this.commonservice.isUpdate){
-      this.updateForm.disable();
-    } else if(this.commonservice.isWrite) {
+    if(!this.commonservice.isUpdate && this.commonservice.isWrite){
       this.updateForm.enable();
+    }else if(!this.commonservice.isUpdate){
+      this.updateForm.disable();
     }
   }
 
