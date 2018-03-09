94a0bc17936051ffcb1046c77e7af6b59950914b
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-03-09 12:36:58 +0800

    updated

--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -51,6 +51,14 @@ export class CommonService {
     arrRight: 'fa fa-angle-right'
   }
 
+
+  pageSize =  [
+    {"id": 1, "size": 10},
+    {"id": 2, "size": 25},
+    {"id": 3, "size": 50}
+  ];
+  defaultPageSize = this.pageSize[0].size;
+
   // tslint:disable-next-line:max-line-length
   constructor(
     public http: Http,
