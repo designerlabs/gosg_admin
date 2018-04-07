8ae1a2a357b64b7541a5f70c04029d2d76a93fd2
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-04-07 15:14:51 +0800

    updated:

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import 'hammerjs';
+import * as $ from 'jquery';
+window["$"] = $;
+window["jQuery"] = $;
 
 if (environment.production) {
   enableProdMode();
