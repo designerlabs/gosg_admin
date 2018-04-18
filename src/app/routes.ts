7d3c8d9b70af5d7a40c3583ab736c3423c4a306c
redza.rosli <redza.rosli@mimos.my>
2018-04-18 16:05:38 +0800

    fixed conflicts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -106,15 +106,12 @@ import { ContentpublishertblComponent } from './contentpublisher/contentpublishe
 import { ContentpublisherComponent } from './contentpublisher/contentpublisher.component';
 import { ParticipationtblComponent } from './participation/participationtbl/participationtbl.component';
 import { ParticipationComponent } from './participation/participation.component';
-<<<<<<< HEAD
 import { DigitalservicetblComponent } from './digitalservice/digitalservicetbl/digitalservicetbl.component';
 import { DigitalserviceComponent } from './digitalservice/digitalservice.component';
 import { DigitalservicetypetblComponent } from './digitalservice/digitalservicetype/digitalservicetypetbl/digitalservicetypetbl.component';
 import { DigitalservicetypeComponent } from './digitalservice/digitalservicetype/digitalservicetype.component';
-=======
 import { ParticipationpublishertblComponent } from './participationpublisher/participationpublishertbl/participationpublishertbl.component';
 import { ParticipationpublisherComponent } from './participationpublisher/participationpublisher.component';
->>>>>>> 4c68aa2a2771d15f1783c5c7ced9143350e3b664
 
 export const appRoutes: Routes = [
 {path: 'index', component: RightcontentComponent},
