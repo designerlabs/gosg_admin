fb4d4631ef2f86437effbe06a6374bad98dd4dc6
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-05-03 18:21:33 +0800

    updated

--- src/app/participation/participationtbl/participationtbl.component.ts
+++ src/app/participation/participationtbl/participationtbl.component.ts
@@ -8,6 +8,8 @@ import { ToastrService } from 'ngx-toastr';
 import { DialogsService } from '../../dialogs/dialogs.service';
 import { TranslateService } from '@ngx-translate/core';
 import { LangChangeEvent } from '@ngx-translate/core';
+import { FormControl, FormGroup, Validators, FormBuilder  } from '@angular/forms';
+import { OwlDateTimeInputDirective } from 'ng-pick-datetime/date-time/date-time-picker-input.directive';
 
 @Component({
   selector: 'app-participationtbl',
@@ -18,6 +20,7 @@ import { LangChangeEvent } from '@ngx-translate/core';
 export class ParticipationtblComponent implements OnInit {
   selectedItem = [];
 
+  updateForm: FormGroup;
   participantData: Object;
   participantList = null;
   displayedColumns: any;
@@ -37,9 +40,24 @@ export class ParticipationtblComponent implements OnInit {
   lang:any;
   languageId: any;
   public loading = false;
-  nameStatus=1;
+  // nameStatus=1;
   keywordVal="";
   recordTable = null;
+
+  keywordVal="";
+  public kataKunci: FormControl;
+  public nameStatus: FormControl;
+
+  dateFormatExample = "dd/mm/yyyy h:i:s";
+  events: string[] = [];
+  publishdt:number;  
+  enddt: number;
+  publish: FormControl
+  endD: FormControl  
+  disableSearch = false;
+  newPublishD: any;
+  newEndD: any;
+  valkey = false;
 
   showNoData = false;
 
@@ -49,9 +67,12 @@ export class ParticipationtblComponent implements OnInit {
   dataSource = new MatTableDataSource<object>(this.participantList);
 
   applyFilter(e) {
+
+    this.nameStatus = this.updateForm.get('nameStatus').value;
+    let d = this.updateForm.get('publish').value;
     
     if(e){
-      this.getFilterList(this.pageCount, this.participantPageSize, e, this.nameStatus);
+      this.getFilterList(this.pageCount, this.participantPageSize, e, this.nameStatus, d);
     }
     else{
       this.getParticipantsData(this.pageCount, this.participantPageSize);
@@ -60,12 +81,17 @@ export class ParticipationtblComponent implements OnInit {
 
   resetSearch() {
     this.getParticipantsData(this.pageCount, this.participantPageSize);
+    this.updateForm.get('kataKunci').setValue(null); 
+    this.valkey = false;
   }
 
   filterStatus(e){
+
+    this.keywordVal = this.updateForm.get('kataKunci').value;
+    let d = this.updateForm.get('publish').value;
     
-    if(this.keywordVal != ""){
-      this.getFilterList(this.pageCount, this.participantPageSize, this.keywordVal, e.value);
+    if(this.keywordVal != null){
+      this.getFilterList(this.pageCount, this.participantPageSize, this.keywordVal, e.value, d);
     }
 
     else{
@@ -100,7 +126,7 @@ export class ParticipationtblComponent implements OnInit {
     });
     if(!this.languageId){
       this.languageId = localStorage.getItem('langID');
-      this.getParticipantsData(this.pageCount, this.participantPageSize);
+      //this.getParticipantsData(this.pageCount, this.participantPageSize);
       this.commonservice.getModuleId();
     }
 
@@ -108,6 +134,22 @@ export class ParticipationtblComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.nameStatus = new FormControl();
+    this.kataKunci = new FormControl();
+    this.publish = new FormControl();
+    this.endD = new FormControl ();
+
+    this.updateForm = new FormGroup({   
+      
+      nameStatus: this.nameStatus,
+      kataKunci: this.kataKunci,
+      endD: this.endD,
+      publish: this.publish
+    });
+    
+    this.updateForm.get('nameStatus').setValue(1);
+
     this.displayedColumns = ['cbox','no','slideTitle', 'sliderDescription', 'slideActiveFlag', 'slideDraft', 'slideAction'];
     this.commonservice.getModuleId();
     this.getParticipantsData(this.pageCount, this.participantPageSize);
@@ -121,22 +163,43 @@ export class ParticipationtblComponent implements OnInit {
   // get e-participation Data 
   getParticipantsData(page, size) {
 
-    let generalUrl = ""
+    let generalUrl = "";
+    let a = this.updateForm.get('nameStatus').value;
 
-    if(this.nameStatus == 1){
-      generalUrl = 'e-participation/creator/state/all';
+    if(a == 1){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/creator/state/all';
+      }
+      else{
+        generalUrl = 'e-participation/creator/state/all/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 2){
-      generalUrl = 'e-participation/creator/state/draft';
+    else if(a == 2){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/creator/state/draft';
+      }
+      else{
+        generalUrl = 'e-participation/creator/state/draft/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 3){
-      generalUrl = 'e-participation/creator/state/pending';
+    else if(a == 3){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/creator/state/pending';
+      }
+      else{
+        generalUrl = 'e-participation/creator/state/pending/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 4){
-      generalUrl = 'e-participation/creator/state/approved';
+    else if(a == 4){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/creator/state/approved';
+      }
+      else{
+        generalUrl = 'e-participation/creator/state/approved/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
     
     this.loading = true;
@@ -170,28 +233,49 @@ export class ParticipationtblComponent implements OnInit {
       });
   }
 
-  getFilterList(page, size, keyword, valStatus) {
+  getFilterList(page, size, keyword, valStatus, dateP) {
     this.participantList = null;
 
     let generalUrl = "";
 
     if(valStatus == 1){
-      generalUrl = 'e-participation/creator/search/state/all';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/creator/search/state/all';
+      }
+      else{
+        generalUrl = 'e-participation/creator/search/state/all/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 2){
-      generalUrl = 'e-participation/creator/search/state/draft';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/creator/search/state/draft';
+      }
+      else{
+        generalUrl = 'e-participation/creator/search/state/draft/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 3){
-      generalUrl = 'e-participation/creator/search/state/pending';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/creator/search/state/pending';
+      }
+      else{
+        generalUrl = 'e-participation/creator/search/state/pending/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 4){
-      generalUrl = 'e-participation/creator/search/state/approved';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/creator/search/state/approved';
+      }
+      else{
+        generalUrl = 'e-participation/creator/search/state/approved/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
     
     if(keyword != "" && keyword != null && keyword.length != null && keyword.length >= 3) {
+      this.valkey = true;
       this.loading = true;
       this.commonservice.readProtected(generalUrl,page, size, keyword).subscribe(
         // this.http.get(this.dataUrl).subscribe(
@@ -227,6 +311,106 @@ export class ParticipationtblComponent implements OnInit {
           this.loading = false;
       });
     }
+  }
+
+  publishEvent(type: string, event: OwlDateTimeInputDirective<Date>) { 
+  
+    this.events = [];
+    this.events.push(`${event.value}`);
+    this.publishdt = new Date(this.events[0]).getTime();
+    this.dateFormatExample = "";    
+
+    if(this.publishdt>this.enddt || this.enddt == undefined || this.enddt == null){
+      this.enddt = new Date(this.events[0]).getTime();
+      this.updateForm.get('endD').setValue(new Date(this.enddt).toISOString());
+      this.enddt = null;
+      this.disableSearch = true;
+    }    
+
+    else{
+      this.disableSearch = false;
+    }
+  }
+
+  endEvent(type: string, event: OwlDateTimeInputDirective<Date>) { 
+
+    this.events = [];
+    this.events.push(`${event.value}`);
+    this.enddt = new Date(this.events[0]).getTime();    
+    this.dateFormatExample = ""; 
+
+    if(this.publishdt>this.enddt || this.publishdt == undefined || this.publishdt == null){
+      this.publishdt = new Date(this.events[0]).getTime();
+      this.updateForm.get('publish').setValue(new Date(this.publishdt).toISOString());
+      this.publishdt = null;
+      this.disableSearch = true;
+    }
+
+    else{
+      this.disableSearch = false;
+    }
+  }
+
+  search(){
+    let year, month, day;   
+    
+    let e = '';
+    
+    if(this.publishdt != undefined){
+      this.events = [];
+      var d = new Date(this.publishdt); 
+      this.events.push(`${d}`);
+
+      year = new Date(this.events[0]).getFullYear();
+      month = new Date(this.events[0]).getMonth()+1;
+      day = new Date(this.events[0]).getDate();
+
+      this.newPublishD = year+"-"+month+"-"+day;
+    }
+
+    if(this.enddt != undefined){
+    
+      this.events = [];
+      var d = new Date(this.enddt); 
+      this.events.push(`${d}`);
+
+      year = new Date(this.events[0]).getFullYear();
+      month = new Date(this.events[0]).getMonth()+1;
+      day = new Date(this.events[0]).getDate();
+      
+      this.newEndD = year+"-"+month+"-"+day;
+    }
+
+    this.nameStatus = this.updateForm.get('nameStatus').value;
+    this.keywordVal = this.updateForm.get('kataKunci').value;
+
+    if(this.keywordVal != undefined || this.keywordVal != null){
+   
+      this.getFilterList(this.pageCount, this.participantPageSize, this.keywordVal, this.nameStatus, this.newPublishD);
+    }
+
+    else if(this.keywordVal == undefined || this.keywordVal == null){
+    
+      this.getParticipantsData(this.pageCount, this.participantPageSize);
+    }
+
+    console.log("Publish: "+this.publishdt);
+    console.log("End: "+this.enddt);
+    console.log("NEW Publish: "+this.newPublishD);
+    console.log("NEW End: "+this.newEndD);
+    console.log(this.updateForm.get('publish').value);
+  }
+
+  clearDate() {
+    this.newPublishD = undefined;
+    this.newEndD = undefined;
+    this.publishdt = undefined;
+    this.enddt = undefined;
+    this.disableSearch = false;
+    this.updateForm.get('publish').setValue(null);
+    this.updateForm.get('endD').setValue(null);
+
+    //this.getGalleryData(this.pageCount, this.galleryPageSize);
   }
 
   paginatorL(page) {
--- src/app/participationpublisher/participationpublishertbl/participationpublishertbl.component.ts
+++ src/app/participationpublisher/participationpublishertbl/participationpublishertbl.component.ts
@@ -8,6 +8,8 @@ import { ToastrService } from 'ngx-toastr';
 import { DialogsService } from '../../dialogs/dialogs.service';
 import { TranslateService } from '@ngx-translate/core';
 import { LangChangeEvent } from '@ngx-translate/core';
+import { FormControl, FormGroup, Validators, FormBuilder  } from '@angular/forms';
+import { OwlDateTimeInputDirective } from 'ng-pick-datetime/date-time/date-time-picker-input.directive';
 
 @Component({
   selector: 'app-participationpublishertbl',
@@ -20,6 +22,8 @@ export class ParticipationpublishertblComponent implements OnInit {
   arrStatus = [];
   selectedItem = [];
   flagApprove: boolean;
+
+  updateForm: FormGroup;
 
   participantData: Object;
   participantList = null;
@@ -40,9 +44,23 @@ export class ParticipationpublishertblComponent implements OnInit {
   lang:any;
   languageId: any;
   public loading = false;
-  nameStatus=1;
+  // nameStatus=1;
   keywordVal="";
   recordTable = null;
+
+  public kataKunci: FormControl;
+  public nameStatus: FormControl;
+
+  dateFormatExample = "dd/mm/yyyy h:i:s";
+  events: string[] = [];
+  publishdt:number;  
+  enddt: number;
+  publish: FormControl
+  endD: FormControl  
+  disableSearch = false;
+  newPublishD: any;
+  newEndD: any;
+  valkey = false;
 
   showNoData = false;
 
@@ -52,9 +70,12 @@ export class ParticipationpublishertblComponent implements OnInit {
   dataSource = new MatTableDataSource<object>(this.participantList);
 
   applyFilter(e) {
+
+    this.nameStatus = this.updateForm.get('nameStatus').value;
+    let d = this.updateForm.get('publish').value;
     
     if(e){
-      this.getFilterList(this.pageCount, this.participantPageSize, e, this.nameStatus);
+      this.getFilterListPP(this.pageCount, this.participantPageSize, e, this.nameStatus, d);
     }
     else{
       this.getParticipantsData(this.pageCount, this.participantPageSize);
@@ -63,12 +84,17 @@ export class ParticipationpublishertblComponent implements OnInit {
 
   resetSearch() {
     this.getParticipantsData(this.pageCount, this.participantPageSize);
+    this.updateForm.get('kataKunci').setValue(null); 
+    this.valkey = false;
   }
 
   filterStatus(e){
 
-    if(this.keywordVal != ""){
-      this.getFilterList(this.pageCount, this.participantPageSize, this.keywordVal, e.value);
+    this.keywordVal = this.updateForm.get('kataKunci').value;
+    let d = this.updateForm.get('publish').value;
+
+    if(this.keywordVal != null){
+      this.getFilterListPP(this.pageCount, this.participantPageSize, this.keywordVal, e.value, d);
     }
 
     else{
@@ -105,7 +131,7 @@ export class ParticipationpublishertblComponent implements OnInit {
     });
     if(!this.languageId){
       this.languageId = localStorage.getItem('langID');
-      this.getParticipantsData(this.pageCount, this.participantPageSize);
+      //this.getParticipantsData(this.pageCount, this.participantPageSize);
       this.commonservice.getModuleId();
     }
 
@@ -113,6 +139,22 @@ export class ParticipationpublishertblComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.nameStatus = new FormControl();
+    this.kataKunci = new FormControl();
+    this.publish = new FormControl();
+    this.endD = new FormControl ();
+
+    this.updateForm = new FormGroup({   
+      
+      nameStatus: this.nameStatus,
+      kataKunci: this.kataKunci,
+      endD: this.endD,
+      publish: this.publish
+    });
+    
+    this.updateForm.get('nameStatus').setValue(1);
+
     this.displayedColumns = ['cbox','no','slideTitle', 'sliderDescription', 'slideActiveFlag', 'slideDraft', 'slideAction'];
     this.commonservice.getModuleId();
     this.getParticipantsData(this.pageCount, this.participantPageSize);
@@ -127,21 +169,42 @@ export class ParticipationpublishertblComponent implements OnInit {
   getParticipantsData(page, size) {
 
     let generalUrl = ""
+    let a = this.updateForm.get('nameStatus').value;
 
-    if(this.nameStatus == 1){
-      generalUrl = 'e-participation/publisher/state/all';
+    if(a == 1){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/publisher/state/all';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/state/all/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 2){
-      generalUrl = 'e-participation/publisher/state/draft';
+    else if(a == 2){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/publisher/state/draft';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/state/draft/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 3){
-      generalUrl = 'e-participation/publisher/state/pending';
+    else if(a == 3){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/publisher/state/pending';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/state/pending/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
-    else if(this.nameStatus == 4){
-      generalUrl = 'e-participation/publisher/state/approved';
+    else if(a == 4){
+      if(this.newPublishD == undefined || this.newPublishD == null){
+        generalUrl = 'e-participation/publisher/state/approved';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/state/approved/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
     
     this.loading = true;
@@ -175,28 +238,49 @@ export class ParticipationpublishertblComponent implements OnInit {
       });
   }
 
-  getFilterList(page, size, keyword, valStatus) {
+  getFilterListPP(page, size, keyword, valStatus, dateP) {
     this.participantList = null;
 
     let generalUrl = "";
 
     if(valStatus == 1){
-      generalUrl = 'e-participation/publisher/search/state/all';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/publisher/search/state/all';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/search/state/all/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 2){
-      generalUrl = 'e-participation/publisher/search/state/draft';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/publisher/search/state/draft';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/search/state/draft/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 3){
-      generalUrl = 'e-participation/publisher/search/state/pending';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/publisher/search/state/pending';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/search/state/pending/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
 
     else if(valStatus == 4){
-      generalUrl = 'e-participation/publisher/search/state/approved';
+      if(dateP == undefined || this.newPublishD == undefined){
+        generalUrl = 'e-participation/publisher/search/state/approved';
+      }
+      else{
+        generalUrl = 'e-participation/publisher/search/state/approved/'+this.newPublishD+"/"+this.newEndD;
+      }
     }
     
     if(keyword != "" && keyword != null && keyword.length != null && keyword.length >= 3) {
+      this.valkey = true;
       this.loading = true;
       this.commonservice.readProtected(generalUrl,page, size, keyword).subscribe(
         // this.http.get(this.dataUrl).subscribe(
@@ -232,6 +316,106 @@ export class ParticipationpublishertblComponent implements OnInit {
           this.loading = false;
       });
     }
+  }
+
+  publishEvent(type: string, event: OwlDateTimeInputDirective<Date>) { 
+  
+    this.events = [];
+    this.events.push(`${event.value}`);
+    this.publishdt = new Date(this.events[0]).getTime();
+    this.dateFormatExample = "";    
+
+    if(this.publishdt>this.enddt || this.enddt == undefined || this.enddt == null){
+      this.enddt = new Date(this.events[0]).getTime();
+      this.updateForm.get('endD').setValue(new Date(this.enddt).toISOString());
+      this.enddt = null;
+      this.disableSearch = true;
+    }    
+
+    else{
+      this.disableSearch = false;
+    }
+  }
+
+  endEvent(type: string, event: OwlDateTimeInputDirective<Date>) { 
+
+    this.events = [];
+    this.events.push(`${event.value}`);
+    this.enddt = new Date(this.events[0]).getTime();    
+    this.dateFormatExample = ""; 
+
+    if(this.publishdt>this.enddt || this.publishdt == undefined || this.publishdt == null){
+      this.publishdt = new Date(this.events[0]).getTime();
+      this.updateForm.get('publish').setValue(new Date(this.publishdt).toISOString());
+      this.publishdt = null;
+      this.disableSearch = true;
+    }
+
+    else{
+      this.disableSearch = false;
+    }
+  }
+
+  search(){
+    let year, month, day;   
+    
+    let e = '';
+    
+    if(this.publishdt != undefined){
+      this.events = [];
+      var d = new Date(this.publishdt); 
+      this.events.push(`${d}`);
+
+      year = new Date(this.events[0]).getFullYear();
+      month = new Date(this.events[0]).getMonth()+1;
+      day = new Date(this.events[0]).getDate();
+
+      this.newPublishD = year+"-"+month+"-"+day;
+    }
+
+    if(this.enddt != undefined){
+    
+      this.events = [];
+      var d = new Date(this.enddt); 
+      this.events.push(`${d}`);
+
+      year = new Date(this.events[0]).getFullYear();
+      month = new Date(this.events[0]).getMonth()+1;
+      day = new Date(this.events[0]).getDate();
+      
+      this.newEndD = year+"-"+month+"-"+day;
+    }
+
+    this.nameStatus = this.updateForm.get('nameStatus').value;
+    this.keywordVal = this.updateForm.get('kataKunci').value;
+
+    if(this.keywordVal != undefined || this.keywordVal != null){
+   
+      this.getFilterListPP(this.pageCount, this.participantPageSize, this.keywordVal, this.nameStatus, this.newPublishD);
+    }
+
+    else if(this.keywordVal == undefined || this.keywordVal == null){
+    
+      this.getParticipantsData(this.pageCount, this.participantPageSize);
+    }
+
+    console.log("Publish: "+this.publishdt);
+    console.log("End: "+this.enddt);
+    console.log("NEW Publish: "+this.newPublishD);
+    console.log("NEW End: "+this.newEndD);
+    console.log(this.updateForm.get('publish').value);
+  }
+
+  clearDate() {
+    this.newPublishD = undefined;
+    this.newEndD = undefined;
+    this.publishdt = undefined;
+    this.enddt = undefined;
+    this.disableSearch = false;
+    this.updateForm.get('publish').setValue(null);
+    this.updateForm.get('endD').setValue(null);
+
+    //this.getGalleryData(this.pageCount, this.galleryPageSize);
   }
 
   paginatorL(page) {
