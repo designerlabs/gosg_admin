5804f105216b29d1e842f479652bbb83e9717d29
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-06-20 17:41:08 +0800

    updated

--- src/app/feedback/feedbacktype/feedbacktypetbl/feedbacktypetbl.component.ts
+++ src/app/feedback/feedbacktype/feedbacktypetbl/feedbacktypetbl.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, Inject, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Inject, ViewChild, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder  } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { APP_CONFIG, AppConfig } from '../../../config/app.config.module';
@@ -9,6 +9,8 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { ToastrService } from 'ngx-toastr';
 import {TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { DialogsService } from '../../../dialogs/dialogs.service';
+import { ISubscription } from 'rxjs/Subscription';
+import { NavService } from '../../../nav/nav.service';
 
 @Component({
   selector: 'app-feedbacktypetbl',
@@ -17,7 +19,7 @@ import { DialogsService } from '../../../dialogs/dialogs.service';
   encapsulation: ViewEncapsulation.None
 })
 
-export class FeedbacktypetblComponent implements OnInit {
+export class FeedbacktypetblComponent implements OnInit, OnDestroy {
 
   public loading = false;
   recordList = null;
@@ -33,6 +35,7 @@ export class FeedbacktypetblComponent implements OnInit {
   seqPageSize = 0 ;
 
   dataUrl: any;  
+  lang: any;
   public languageId: any;
   recordTable = null;
   
@@ -40,6 +43,11 @@ export class FeedbacktypetblComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   
   dataSource = new MatTableDataSource<object>(this.recordList);
+
+  private subscriptionLang: ISubscription;
+  private subscriptionContentCreator: ISubscription;
+  private subscriptionCategoryC: ISubscription;
+  private subscriptionRecordListC: ISubscription;
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
@@ -53,42 +61,60 @@ export class FeedbacktypetblComponent implements OnInit {
     private router: Router, 
     private toastr: ToastrService,
     private translate: TranslateService,
+    private navservice: NavService,
     private dialogsService: DialogsService) {
 
-    /* LANGUAGE FUNC */
-    translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      translate.get('HOME').subscribe((res: any) => {
-        this.commonservice.readPortal('language/all').subscribe((data:any) => {
-          let getLang = data.list;
-          let myLangData =  getLang.filter(function(val) {
-            if(val.languageCode == translate.currentLang){
-              this.lang = val.languageCode;
-              this.languageId = val.languageId;
-              this.getRecordList(this.pageCount, this.pageSize);
-              this.commonservice.getModuleId();
-            }
-          }.bind(this));
-        })
-      });
+      /* LANGUAGE FUNC */
+    this.subscriptionLang = translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      const myLang = translate.currentLang;
+
+      if (myLang == 'en') {
+        translate.get('HOME').subscribe((res: any) => {
+          this.lang = 'en';
+          this.languageId = 1;
+        });
+      }
+
+      if (myLang == 'ms') {
+        translate.get('HOME').subscribe((res: any) => {
+          this.lang = 'ms';
+          this.languageId = 2;
+        });
+      }
+      if (this.navservice.flagLang) {
+        console.log("constructor")
+        this.getRecordList(this.pageCount, this.pageSize);
+        this.commonservice.getModuleId();
+      }
+
     });
-    if(!this.languageId){
-      this.languageId = localStorage.getItem('langID');
-      this.getRecordList(this.pageCount, this.pageSize);
-      this.commonservice.getModuleId();
-    }
     /* LANGUAGE FUNC */
+
+  }
+
+  ngOnDestroy() {
+    this.subscriptionLang.unsubscribe();
+    // this.subscriptionContentCreator.unsubscribe();
+    // this.subscriptionCategoryC.unsubscribe();
+    // this.subscriptionRecordListC.unsubscribe();
   }
 
   ngOnInit() {
 
-    //this.getRecordList(this.pageCount, this.pageSize);
+    if (!this.languageId) {
+      this.languageId = localStorage.getItem('langID');
+    } else {
+      this.languageId = 1;
+    }
+
+    this.getRecordList(this.pageCount, this.pageSize);
     this.commonservice.getModuleId();
   }
 
   getRecordList(count, size) {
   
     this.loading = true;
-    this.commonservice.readPortal('feedback/type', count, size)
+    this.commonservice.readPortal('feedback/type', count, size, '', this.languageId)
     .subscribe(data => {
 
       this.commonservice.errorHandling(data, (function(){
