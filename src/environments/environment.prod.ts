c50541fdf6ad3304f7e119d369d14d3630161827
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-09-19 15:53:54 +0800

    change environment for staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   staging:false,
-  uapURL : '/',
+  uapURL : 'https://gosgstag.malaysia.gov.my/',
   stagURL: 'https://gosgstag.malaysia.gov.my/',
-  mediaURL: '/',
-  mockApiURL :'/',
-  baseURL: '/',
-  mibisURL: '/'
+  mediaURL: 'https://gosgstag.malaysia.gov.my/',
+  mockApiURL :'https://gosgstag.malaysia.gov.my/',
+  baseURL: 'https://gosgstag.malaysia.gov.my/',
+  mibisURL: 'https://stagam.malaysia.gov.my/'
 };
