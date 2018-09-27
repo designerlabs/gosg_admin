0cf557a067fde64edba4430dc09a33d41fae5633
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-09-27 22:52:49 +0800

    i

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,11 @@
 export const environment = {
   production: false,
   staging: false,
-  uapURL : '/',
-  stagURL: '/',
-  mediaURL: '/',
-  mockApiURL :'/',
-  baseURL: '/',
-  mibisURL: '/'
+
+  uapURL : 'https://stagam.malaysia.gov.my/',
+stagURL: 'https://gosgstag.malaysia.gov.my/',
+mediaURL: 'https://gosgstag.malaysia.gov.my/',
+ mockApiURL :'https://gosgstag.malaysia.gov.my/',
+ baseURL: 'https://gosgstag.malaysia.gov.my/',
+mibisURL: 'https://stagam.malaysia.gov.my/'
 };
