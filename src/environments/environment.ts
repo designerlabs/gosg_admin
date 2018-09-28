2d4ced89fd03049035311e881673f5128325e404
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-09-28 15:08:53 +0800

    environment changed back to dev mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,10 @@
 export const environment = {
   production: false,
   staging: false,
-
-  uapURL : 'https://stagam.malaysia.gov.my/',
-stagURL: 'https://gosgstag.malaysia.gov.my/',
-mediaURL: 'https://gosgstag.malaysia.gov.my/',
- mockApiURL :'https://gosgstag.malaysia.gov.my/',
- baseURL: 'https://gosgstag.malaysia.gov.my/',
-mibisURL: 'https://stagam.malaysia.gov.my/'
+  uapURL : '',
+  stagURL: '',
+  mediaURL: '',
+  mockApiURL :'',
+  baseURL: '',
+  mibisURL: ''
 };
